d075877d820e958a0f6c132481e9c9e430ee06ee
tboonma <tboonma@hotmail.com>
2021-09-18 17:10:27 +0700

    Done #8 and #9 in Question model

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,26 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Date published')
+    end_date = models.DateTimeField('End date', default=None, blank=True, null=True)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.now()
+        return self.pub_date <= now
+
+    def can_vote(self):
+        now = timezone.now()
+        if self.end_date is None:
+            return self.pub_date < now
+        return self.pub_date <= now < self.end_date
 
     def __str__(self):
         return self.question_text
