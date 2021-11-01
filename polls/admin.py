2d17cac7c71261fdaf01915dfb3516b11750d9e4
tboonma <tboonma@hotmail.com>
2021-11-01 19:30:47 +0700

    Register Vote model

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Question, Choice, Vote
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Vote)
