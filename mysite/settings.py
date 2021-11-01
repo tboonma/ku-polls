21e3bc674f291c2639dff336fc9620a13f8e6d05
tboonma <tboonma@hotmail.com>
2021-11-01 22:36:07 +0700

    Add auth strategy

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication strategies
+AUTHENTICATION_BACKENDS = (
+    # username/password authentication
+   'django.contrib.auth.backends.ModelBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
