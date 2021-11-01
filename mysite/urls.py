c21faf4c28ed3442294ffc09fd5fee5a3e6d1777
tboonma <tboonma@hotmail.com>
2021-11-01 23:26:28 +0700

    Fix bug by missing,

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
     path('', include('polls.urls', namespace="polls"))
 ]
